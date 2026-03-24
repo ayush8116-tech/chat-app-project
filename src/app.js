d54af70e8fdf6e9d378bc9e1c74126c34e62ee5e
Ayush Verma <vermajiii8116@gmail.com>
2026-03-24 07:13:56 +0530

    tries to add some node thing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import {  Hono } from "hono";
+import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/deno";
 
@@ -7,13 +7,13 @@ export const createApp = () => {
 
   app.use(logger());
 
-  app.post("/github-webhook", async(c) => {
-    console.log("comming....");
+  app.post("/github-webhook", async (c) => {
+    console.log("comming in chat-app....");
     const body = await c.req.json();
     console.log(body);
-      
+
     return c.text("done");
-  })
+  });
 
   app.get("*", serveStatic({ root: "public" }));
   return app;
